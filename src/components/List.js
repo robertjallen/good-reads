b9bcc97e75141048a8d57bb43776b78d54bd1895
robertjallen <bobbidigi34@gmail.com>
2022-04-14 16:54:00 -0400

    add update title on list

--- src/components/List.js
+++ src/components/List.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function List(props) {
   return (
 	<div className='list'>
-		<h3>{props.pick} is the pick of the day</h3>
+		<h3>{props.pick} books</h3>
 		{props.data.map((book, index) => (
 			<div className='card'>
 				<p key={index}>{book.title}</p>
